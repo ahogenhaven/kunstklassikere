66100bee0d54572cacb91ea529c8145054197ee1
ahogenhaven <andreashogen@gmail.com>
2014-08-27 17:52:56 +0200

    sftp upload er fucked

--- templates/header.php
+++ templates/header.php
@@ -1,51 +1,6 @@
 <header class="banner headerclass" role="banner">
 <?php if (kadence_display_topbar()) : ?>
-  <!--<section id="topbar" class="topclass">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-sm-6 kad-topbar-left">
-          <div class="topbarmenu clearfix">
-          <?php if (has_nav_menu('topbar_navigation')) :
-              wp_nav_menu(array('theme_location' => 'topbar_navigation', 'menu_class' => 'sf-menu'));
-            endif;?>
-            <?php if(kadence_display_topbar_icons()) : ?>
-            <div class="topbar_social">
-              <ul>
-                <?php global $virtue; $top_icons = $virtue['topbar_icon_menu'];
-                foreach ($top_icons as $top_icon) {
-                  if(!empty($top_icon['target']) && $top_icon['target'] == 1) {$target = '_blank';} else {$target = '_self';}
-                  echo '<li><a href="'.$top_icon['link'].'" target="'.$target.'" title="'.esc_attr($top_icon['title']).'" data-toggle="tooltip" data-placement="bottom" data-original-title="'.esc_attr($top_icon['title']).'">';
-                  if($top_icon['url'] != '') echo '<img src="'.$top_icon['url'].'"/>' ; else echo '<i class="'.$top_icon['icon_o'].'"></i>';
-                  echo '</a></li>';
-                } ?>
-              </ul>
-            </div>
-          <?php endif; ?>
-            <?php global $virtue; if(isset($virtue['show_cartcount'])) {
-               if($virtue['show_cartcount'] == '1') { 
-                if (class_exists('woocommerce')) {
-                  global $woocommerce; ?>
-                    <ul class="kad-cart-total">
-                      <li>
-                      <a class="cart-contents" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php esc_attr_e('View your shopping cart', 'woocommerce'); ?>">
-                          <i class="icon-shopping-cart" style="padding-right:5px;"></i> <?php _e('Your Cart', 'virtue');?> <span class="kad-cart-dash">-</span> <?php echo $woocommerce->cart->get_cart_total(); ?>
-                      </a>
-                    </li>
-                  </ul>
-                <?php } } }?>
-          </div>
-        </div><!-- close col-md-6 --> 
-       <!--
-        <div class="col-md-6 col-sm-6 kad-topbar-right">
-          <div id="topbar-search" class="topbar-widget">
-            <?php if(kadence_display_topbar_widget()) { if(is_active_sidebar('topbarright')) { dynamic_sidebar(__('Topbar Widget', 'virtue')); } 
-              } else { if(kadence_display_top_search()) {get_search_form();} 
-          } ?>
-        </div>
-        </div> 
-      </div> 
-    </div> 
-  </section> > -->
+
 <?php endif; ?>
 <?php global $virtue; if(isset($virtue['logo_layout'])) {
   if($virtue['logo_layout'] == 'logocenter') {$logocclass = 'col-md-12'; $menulclass = 'col-md-12';}
@@ -62,7 +17,7 @@
                          <?php if(!empty($virtue['x2_virtue_logo_upload']['url'])) {?> <img src="<?php echo $virtue['x2_virtue_logo_upload']['url'];?>" class="kad-retina-logo" style="max-height:<?php echo $virtue['x1_virtue_logo_upload']['height'];?>px" /> <?php } ?>
                         </div> <?php } else { bloginfo('name'); } ?>
                         </a>
-              <?php if (isset($virtue['logo_below_text'])) { ?> <p class="kad_tagline belowlogo-text"> <?php bloginfo('description'); ?> </p> <?php }?>
+              <p class="kad_tagline belowlogo-text"><?php bloginfo('description'); ?>  </p>
               </h1>
               <p>
              
